a662edfc8c5070c5519a82fa58386dbcd36d3b93
kanamo-r <r040131@yahoo.co.jp>
2024-03-29 04:07:34 +0900

    task4-2-2完了しました

--- PHP/task4-2-2.php
+++ PHP/task4-2-2.php
@@ -6,6 +6,22 @@
   <title>Document</title>
 </head>
 <body>
-  
+
+<h1>
+<?php
+$month = mt_rand(1, 12);
+
+if ($month >= 3 && $month <= 5) {
+    echo $month . "月は春の季節です。";
+} elseif ($month >= 6 && $month <= 8) {
+    echo $month . "月は夏の季節です。";
+} elseif ($month >= 9 && $month <= 11) {
+    echo $month . "月は秋の季節です。";
+} else {
+    echo $month . "月は冬の季節です。";
+}
+?>
+</h1>
+
 </body>
 </html>
