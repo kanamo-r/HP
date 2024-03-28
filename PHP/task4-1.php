ec56e7a59416e994c8bf61809784a489794db0a3
kanamo-r <r040131@yahoo.co.jp>
2024-03-29 03:44:03 +0900

    task4-1完了しました

--- PHP/task4-1.php
+++ PHP/task4-1.php
@@ -6,6 +6,25 @@
   <title>Document</title>
 </head>
 <body>
-  
+
+<h1>
+今日の運勢は"<?php
+$rand = mt_rand(1, 100);
+
+if ($rand <= 5) {
+    echo '大凶';
+} elseif ($rand <= 20) {
+    echo '凶';
+} elseif ($rand <= 50) {
+    echo '吉';
+} elseif ($rand <= 80) {
+    echo '中吉';
+} else {
+    echo '大吉';
+}
+?>
+"です。
+</h1>
+
 </body>
 </html>
