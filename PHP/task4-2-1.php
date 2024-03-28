d06e08dcbf45365083b14bbb6672df2d77385922
kanamo-r <r040131@yahoo.co.jp>
2024-03-29 04:03:11 +0900

    task4-2-1完了しました

--- PHP/task4-2-1.php
+++ PHP/task4-2-1.php
@@ -6,6 +6,32 @@
   <title>Document</title>
 </head>
 <body>
+
+<h1>
+<?php
+$month = mt_rand(1, 12);
+
+switch ($month) {
+  case 3:
+  case 4:
+  case 5:
+    echo $month . "月は春の季節です。";
+    break;
+  case 6:
+  case 7:
+  case 8:
+    echo $month . "月は夏の季節です。";
+    break;
+  case 9:
+  case 10:
+  case 11:
+    echo $month . "月は秋の季節です。";
+    break;
+  default:
+    echo $month . "月は冬の季節です。";
+}
+?>
+</h1>
   
 </body>
 </html>
