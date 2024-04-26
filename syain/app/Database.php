cf66edfe0ae239655a7888c5f599c9e088b7eee8
kanamo-r <r040131@yahoo.co.jp>
2024-04-26 23:47:59 +0900

    postdata.php更新しました

--- syain/app/Database.php
+++ syain/app/Database.php
@@ -34,4 +34,33 @@ class Database
       exit;
     }
   }
+
+  function getsyain($id)
+  {
+    try {
+      $this->connect();
+      $stmt = $this->pdo->prepare("SELECT * FROM syain WHERE id = ? ;");
+      $stmt->bindParam(1, $id, PDO::PARAM_INT);
+      $member = $stmt->execute();
+      if ($member) {
+        $member = $stmt->fetchAll();
+        if (count($member) == 0) {
+          return null;
+        }
+        return $member[0];
+      }
+      return null;
+    } catch(PDOException $e) {
+      echo $e->getMesseage() . '<br>';
+      exit;
+    }
+  }
+
+  function idexist($id)
+  {
+    if ($this->getsyain($id) != null) {
+      return true;
+    }
+    return false;
+  }
 }
